480128552cb7f87db21669f8d7eef909dd7a9473
Amoq18 <dopegnonamoq18gmail.com>
2020-12-30 18:55:50 +0100

    integration forum en cours

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('', '');
+Route::get('', 'HomeController@index');
+Route::get('forum', 'HomeController@forum');
 
 Route::group(['prefix' => 'admin'], function () {
     Route::get('/', function () {
